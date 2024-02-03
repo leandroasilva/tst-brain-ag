d37a9d3863b36d34fa79f320509205fc9fe90d37
Leandro A Silva <halk_lenson@hotmail.com>
2024-02-03 10:29:45 -0300

    fix: resolve error imports

--- src/app/rural-producer/rural-producer.module.ts
+++ src/app/rural-producer/rural-producer.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UtilsModule } from 'src/infra/providers/utils/utils.module';
+import { PlantedCropsRepositoryModule } from 'src/infra/repositories/planted-crops/planted-crops.module';
 import { RuralProducerRepositoryModule } from 'src/infra/repositories/rural-producer/rural-producer.module';
 
 import { RuralProducerController } from './rural-producer.controller';
 import { RuralProducerService } from './rural-producer.service';
-import { PlantedCropsRepositoryModule } from 'src/infra/repositories/planted-crops/planted-crops.module';
 
 @Module({
   imports: [
